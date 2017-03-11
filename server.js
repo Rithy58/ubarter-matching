3ef7cc829596f2e739e4ea22b4452d6d9953b5e3
SeihakRithy Muth <Rithy58@gmail.com>
2017-03-11 12:57:43 -0500

    added default api request

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var app = express();
 app.set('port', 8080);
 app.use(express.static('./public/'));
 
+app.get('/api/', function(req, res) {
+  res.send('API Page');
+});
+
 var server = app.listen(app.get('port'), function() {
   console.log('Server is running on port', app.get('port'));
 });
