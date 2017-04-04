9aed5f83f9dc3401c7add4254313ebc5d90db420
SeihakRithy Muth <Rithy58@gmail.com>
2017-04-04 10:28:06 -0400

    displays user token on homepage

--- public/app/controllers/homeController.js
+++ public/app/controllers/homeController.js
@@ -1,6 +1,7 @@
 app.controller('homeController', ['$scope', 'authService', '$window', function($scope, authService, $window) {
   $scope.loggedIn = authService.checkLoggedIn();
   $scope.username = authService.getUser().username;
+  $scope.token = authService.getToken();
   $scope.login = function() {
     authService.login($scope.user).then(function(){
       $window.location.href= '/';
